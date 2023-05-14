aace0a551fa74c661ec40577d188cb42eab79a31
salliko <snup41@yandex.ru>
2023-05-15 00:10:32 +0300

    Добавил код работы с БД

--- src/use_cases/profile_service.py
+++ src/use_cases/profile_service.py
@@ -1,24 +1,51 @@
-from models.profile import ProfileModel
+import typing
+
+from sqlalchemy import select, update, delete
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from infrastructure.entities import Profile, engine
+
+
+if typing.TYPE_CHECKING:
+    from models.profile import ProfileModel
 
 
 class ProfileService:
-    def __init__(self):
-        pass
-
-    def create(self):
+    async def create(self, *, profile: 'ProfileModel'):
         """Создать профиль пользователя"""
-        pass
+        prfile_data = Profile(**profile.dict())
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                session.add(prfile_data)
+            await session.commit()
+        return str(profile.user_id)
 
-    def update(self, *, user_id: str, profile: ProfileModel):
+    async def update(self, *, user_id: str, profile: 'ProfileModel'):
         """Обновить профиль пользователя"""
-        pass
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                await session.execute(
+                    update(Profile)
+                    .where(Profile.user_id == user_id)
+                    .values(**profile.dict())
+                )
+            await session.commit()
 
-    def get(self, user_id: str | None = None) -> ProfileModel | list[ProfileModel]:
+    async def get(self, user_id: str | None = None):
         """Получить профиль одного пользователя по индефикатору
         или если индефитор пустой показать всех.
         """
-        pass
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                if user_id:
+                    query = await session.execute(select(Profile).where(Profile.user_id == user_id))
+                else:
+                    query = await session.execute(select(Profile))
+                return query.all()
 
-    def delete(self, user_id: str):
+    async def delete(self, user_id: str):
         """Заморозить профиль пользователя"""
-        pass
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                await session.execute(delete(Profile).where(Profile.user_id == user_id))
+            await session.commit()
